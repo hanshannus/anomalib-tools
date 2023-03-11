984c8d39734655c64e8d68b6292f16cf5a8e2b38
Johannes Hecher <31933940+hanshannus@users.noreply.github.com>
2023-03-11 15:29:33 +0100

    FEAT: rename package

--- anomalibtools/train.py
+++ anomalibtools/train.py
@@ -13,48 +13,25 @@ logger = logging.getLogger("anomalib")
 
 def train(
     config_path: Union[str, Path],
-    data_dir: Union[str, Path] = None,
-    output_dir: Union[str, Path] = None,
-    ckpt_path: Union[str, Path] = None,
-    max_epochs: Union[str, Path] = None,
-) -> str:
+) -> Trainer:
     """Train Anomalib model.
 
     Parameters
     ----------
     config_path : str
         Path to the configuration file.
-    data_dir : str, optional
-        Path to data root directory, by default None
-    output_dir : str, optional
-        Path to output data directory, by default None
-    ckpt_path : str, optional
-        Path to model checkpoint to load, by default None
-    max_epochs : int, optional
-        Maximum number of epochs, by default None
 
     Returns
     -------
-    str
-        Path to the best checkpoint.
+    Trainer
+        Pytorch lightning trainer after training.
     """
     config_path = str(config_path)
-    data_dir = str(data_dir)
-    output_dir = str(output_dir)
-    ckpt_path = str(ckpt_path)
 
     configure_logger(level="INFO")
 
     logger.info("Load configuration file.")
     config = load_config(config_path)
-
-    logger.info("Update config with input parameters.")
-    if data_dir is not None:
-        config.dataset.path = data_dir
-    if output_dir is not None:
-        config.project.path = output_dir
-    if max_epochs is not None:
-        config.trainer.max_epochs = max_epochs
 
     logger.info("Define Datamodule.")
     datamodule = get_datamodule(config)
@@ -73,10 +50,7 @@ def train(
     trainer.fit(
         model=model,
         datamodule=datamodule,
-        ckpt_path=ckpt_path,
+        ckpt_path=config.model.get("ckpt_path", None),
     )
 
-    best_model = trainer.checkpoint_callback.best_model_path
-    logger.info(f"Path to best checkpoint: {best_model}")
-
-    return best_model
+    return trainer
